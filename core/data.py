c47be07969693ebcbbc94df53c20ded720fb9e1b
Nur A.G <nuralgon@gmail.com>
2023-04-01 13:41:52 +0200

    Introduce 4x4 Rook / Shrikhande graphs as a task to validate Theorem 4. (EIGEL-L with Edge info distinguishes SRGs)

--- core/data.py
+++ core/data.py
@@ -16,6 +16,25 @@ from core.data_utils.sbm_cliques import CliqueSBM
 from core.data_utils.tudataset_gin_split import TUDatasetGINSplit
 
 
+class RookShrikhandeDataset(InMemoryDataset):
+    """Implements an in-memory 4x4 Rook and Shrikhande (3-WL, Strongly Regular) dataset
+    with the two graphs.
+
+    This is helpful to study 3-WL expressivity.
+    """
+    ROOK_EDGES = [(0, 1), (0, 2), (0, 3), (0, 4), (0, 8), (0, 12), (1, 2), (1, 3), (1, 5), (1, 9), (1, 13), (2, 3), (2, 6), (2, 10), (2, 14), (3, 7), (3, 11), (3, 15), (4, 5), (4, 6), (4, 7), (4, 8), (4, 12), (5, 6), (5, 7), (5, 9), (5, 13), (6, 7), (6, 10), (6, 14), (7, 11), (7, 15), (8, 9), (8, 10,), (8, 11), (8, 12), (9, 10), (9, 11), (9, 13), (10, 11), (10, 14), (11, 15), (12, 13), (12, 14), (12, 15), (13, 14), (13, 15), (14, 15)]
+    SHRIKHANDE_EDGES = [(0, 1), (0, 3), (0, 4), (0, 5), (0, 12), (0, 15), (1, 2), (1, 5), (1, 6), (1, 12), (1, 13), (2, 3), (2, 6), (2, 7), (2, 13), (2, 14), (3, 4), (3, 7), (3, 14), (3, 15), (4, 5), (4, 7), (4, 8), (4, 9), (5, 6), (5, 9), (5, 10), (6, 7), (6, 10), (6, 11), (7, 8), (7, 11), (8, 9), (8, 11), (8, 12), (8, 13), (9, 10), (9, 13), (9, 14), (10, 11), (10, 14), (10, 15), (11, 12), (11, 15), (12, 13), (12, 15), (13, 14), (14, 15)]
+
+    def __init__(self, root, transform=None):
+        super().__init__('.', transform)
+        x = torch.ones((16, 1)).long()
+        rook_tensor = to_undirected(torch.Tensor([list(x) for x in zip(*self.ROOK_EDGES)]).long())
+        shrik_tensor = to_undirected(torch.Tensor([list(x) for x in zip(*self.SHRIKHANDE_EDGES)]).long())
+
+        rook_data = Data(x=x, edge_index=rook_tensor, y=0)
+        shrik_data = Data(x=x, edge_index=shrik_tensor, y=1)
+        self.data, self.slices = self.collate([rook_data, shrik_data])
+
 
 class ProximityDataset(InMemoryDataset):
     """Implements an in-memory dataset for the K-Proximity datasets
