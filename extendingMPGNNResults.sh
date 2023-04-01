9f3f7f7ecfc2f3b1c76541735cabd8dc29d9d2ae
Nur A.G <nuralgon@gmail.com>
2023-04-01 23:29:48 +0200

    Remove unused reproducibility script

--- extendingMPGNNResults.sh
+++
@@ -1,6 +0,0 @@
-for GNN_TYPE in "CGConv" "NNConv" "TransformerConv" "GINEConv" "GATConv" "GCNConv"; do
-  echo ./runCommandOnGPUMemThreshold.sh "python -m train.zinc model.gnn_type ${GNN_TYPE} best params" 35000
-  echo ./runCommandOnGPUMemThreshold.sh "python -m train.molhiv model.gnn_type ${GNN_TYPE} best params" 35000
-  echo ./runCommandOnGPUMemThreshold.sh "python -m train.proximity task 10 model.gnn_type ${GNN_TYPE} best params" 35000
-done
-
